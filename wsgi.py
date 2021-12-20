cfef4e9411e2351a18050a2bfdcf8c3060606952
vikassood1101 <v.sood@jamiahamdard.ac.in>
2021-12-20 13:02:23 +0530

    .

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,9 @@
-#!/var/www/html/qufind/env/bin python3
 
-import sys
-import site
+#import sys
+#import site
 
-site.addsitedir('/var/www/html/qufind/env/lib/python3.6/site-packages')
+#site.addsitedir('/home/centos/.local/lib/python3.9/site-packages')
 
-sys.path.insert(0, '/var/www/html/qufind')
+#sys.path.insert(0, '/var/www/html/qufind')
 
 from app import app as application
